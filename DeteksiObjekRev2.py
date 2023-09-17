b41e33f5baf1a7e595f582a9debd3f1b69a788e5
abdulaziz2306 <122335709+abdulaziz2306@users.noreply.github.com>
2023-09-17 22:04:59 +0800

    update lagi

--- DeteksiObjekRev2.py
+++ DeteksiObjekRev2.py
@@ -2,16 +2,11 @@ from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput
 
 net = detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = videoSource("csi://0")      # '/dev/video0' for V4L2
-display = videoOutput("display://0") # 'my_video.mp4' for file
+camera = videoSource(640, 480, "/dev/video1")      # '/dev/video0' for V4L2
+display = jetson.utils.glDisplay() # 'my_video.mp4' for file
 
-while display.IsStreaming():
-    img = camera.Capture()
-
-    if img is None: # capture timeout
-        continue
-
-    detections = net.Detect(img)
-    
-    display.Render(img)
-    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+while display.IsOpen():
+    img, width, height = cameraCaptureRGBA()
+    detetctions = net.Detect(img, width, height)
+    display.RenderOnce(img, width, height)
+    display.SetTitle("FPS : ". format(net.GetNetworkFPS()))
